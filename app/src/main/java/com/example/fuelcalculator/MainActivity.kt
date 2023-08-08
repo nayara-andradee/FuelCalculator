03118761387361d760b35a8e2574240d49ebc47d
Anderson <anderson.jerfson@gmail.com>
2023-08-08 19:13:35 -0300

    commit Inserindo MVVM Anderson

--- app/src/main/java/com/example/fuelcalculator/MainActivity.kt
+++ app/src/main/java/com/example/fuelcalculator/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.fuelcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fuelcalculator.presentation.FuelCalculatorViewModel
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+   }
+
 }
