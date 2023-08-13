2f9751bdcb3749828199fc1665cabc65268afe40
Victor Yule <105625896+VictorYule@users.noreply.github.com>
2023-08-13 10:47:43 -0300

    Criacao dialog distance

---
+++ app/src/main/java/com/example/fuelcalculator/presentation/DialogDistance.kt
@@ -0,0 +1,23 @@
+package com.example.fuelcalculator.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.example.fuelcalculator.R
+
+class DialogDistance : DialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.dialog_distance, container)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
+}
